a36b1e5abf22858fc661cd119014cfd223773c69
kehitt <kehitt@users.noreply.github.com>
2022-11-16 20:51:15 +0400

    Fix migration running/compilation

--- mix.exs
+++ mix.exs
@@ -5,8 +5,13 @@ defmodule ApiKeyMgmtUmbrella.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
+  end
+
+  defp aliases do
+    [test: ["compile", "cmd mix test"]]
   end
 
   defp deps do
